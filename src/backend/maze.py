4e3a42f4c7b9ff21269a22abba5d7fb39b927027
somecollagist <johannscott16@gmail.com>
2023-10-22 13:47:11 +0100

    maze?

--- src/backend/maze.py
+++ src/backend/maze.py
@@ -1,12 +1,24 @@
 from backend.backend import Square
+from random import choice as rchoice
 
 MAZE_HEIGHT = 63
 MAZE_WIDTH  = 63
+COORDDELTAS = [(-1,0), (0,1), (1,0), (0,-1)]
+
+# use iterative randomised Prim's algorithm for maze gen
+Maze = [[Square.WALL for x in range(int(MAZE_WIDTH+2))] for y in range(int(MAZE_HEIGHT+2))]
+Walls = []
+
+def AllocateSquareAsFloor(y, x):
+    Maze[y][x] = Square.FLOOR
+    for coorddelta in COORDDELTAS:
+        try:
+            if(Maze[y+coorddelta[1]][x+coorddelta[0]] in [Square.WALL, Square.WALL_VISTED]):
+                Walls.append((y+coorddelta[1], x+coorddelta[0]))
+        except IndexError:
+            continue
 
 def GenerateMaze():
-    # use iterative randomised Prim's algorithm for maze gen
-    Maze = [[Square.WALL for x in range(int(MAZE_WIDTH+2))] for y in range(int(MAZE_HEIGHT+2))]
-
     # add borders
     for x in range(MAZE_WIDTH+2):
         Maze[0][x]              = Square.BORDER
@@ -16,6 +28,21 @@ def GenerateMaze():
         Maze[y][0]              = Square.BORDER
         Maze[y][MAZE_WIDTH+1]   = Square.BORDER
 
-    Maze[1][1] = Square.FLOOR
+    AllocateSquareAsFloor(1,1)
+
+    while(len(Walls) > 0):
+        coords = rchoice(Walls)
+        adjacentfloors = 0
+        for coorddelta in COORDDELTAS:
+            if(adjacentfloors > 1):
+                break
+            if(Maze[coords[0]+coorddelta[0]][coords[1]+coorddelta[1]] == Square.FLOOR):
+                adjacentfloors += 1
+            
+        if(adjacentfloors == 1):
+            AllocateSquareAsFloor(coords[0], coords[1])
+        
+        Walls.remove(coords)
+            
     
     return Maze
