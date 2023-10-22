08847d7ef459fc84dcfd1eb7b4a3eb745a52fa6b
Shikhar-Srivastava-16 <shikhars2006@gmail.com>
2023-10-22 13:04:18 +0100

    finished checkOverlap

--- src/backend/functional.py
+++ src/backend/functional.py
@@ -2,16 +2,18 @@ import pygame
 
 def checkOverlap(tupBox,tupPlayer,widthBox = 1,widthPlayer = 1):
     """
-    Checks for the 
+    Checks for overlap of Player in Box
+    tupBox is centre of Box
+    tupPlayer is centre of Player 
     """
 
     if '''check rBox against lPlay''' tupPlayer[0]-(widthPlayer/2)'''lPlayer''' < tupBox[0]+(widthBox/2)'''rBox''':
         return True
     elif '''check lBox against rPlay''' tupBox[0]-(widthBox/2)'''lBox''' < tupPlayer[0]+(widthPlayer/2)'''rPlay''':
         return True
-    elif #tBox against bPlay:
+    elif '''tBox against bPlay''' tupBox[1]+(widthPlayer/2)'''tBox''' > tupPlayer[1]-(widthPlayer/2)'''bPlay''':
         return True
-    elif #bBox against tPlay:
+    elif '''bBox against tPlay''' tupBox[1]-(widthBox/2)'''bBox''' < tupPlayer[1]+(widthPlayer/2)'''tPlay''':
         return True
     else:
         return False
