8863db49b7790bfe78635e2d1c8f02cfa01eff81
Shikhar-Srivastava-16 <shikhars2006@gmail.com>
2023-10-22 12:29:03 +0100

    created functional.py

--- README.md
+++
@@ -1,2 +0,0 @@
-# HackTheBubble2023
-Hack the Bubble St Andrews 2023 Hackathon for Halloween 
---
+++ src/backend/functional.py
@@ -0,0 +1,17 @@
+import pygame
+
+def checkOverlap(tupBox,tupPlayer):
+    """
+    Checks for the 
+    """
+
+    if #check rBox against lPlay:
+        return True
+    elif #check lBox against rPlay:
+        return True
+    elif #tBox against bPlay:
+        return True
+    elif #bBox against tPlay:
+        return True
+    else:
+        return False
