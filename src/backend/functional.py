d4b65cd8fff0b60bde040aefb0088d9707e239f0
Shikhar-Srivastava-16 <shikhars2006@gmail.com>
2023-10-22 12:54:44 +0100

    changed functional

--- src/backend/functional.py
+++ src/backend/functional.py
@@ -1,13 +1,13 @@
 import pygame
 
-def checkOverlap(tupBox,tupPlayer):
+def checkOverlap(tupBox,tupPlayer,widthBox = 1,widthPlayer = 1):
     """
     Checks for the 
     """
 
-    if #check rBox against lPlay:
+    if '''check rBox against lPlay''' tupPlayer[0]-(widthPlayer/2)'''lPlayer''' < tupBox[0]+(widthBox/2)'''rBox''':
         return True
-    elif #check lBox against rPlay:
+    elif '''check lBox against rPlay''' tupBox[0]-(widthBox/2)'''lBox''' < tupPlayer[0]+(widthPlayer/2)'''rPlay''':
         return True
     elif #tBox against bPlay:
         return True
