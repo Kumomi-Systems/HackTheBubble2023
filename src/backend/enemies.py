9bef176921f994ffdfb48195db160b689a5d9c1a
Shikhar-Srivastava-16 <shikhars2006@gmail.com>
2023-10-22 15:40:25 +0100

    added goblin class

--- src/backend/enemies.py
+++ src/backend/enemies.py
@@ -5,35 +5,46 @@ class Karen:
     def __init__(self, listCoord):
         self.listCoord = listCoord
 
-    def method1(self,varChange,varAxis):
+    def moveKaren(self,varChange,varAxis):
         """
         varChange is external value passed to tell the listCoord how to change
         can be '+ve' or '-ve' 
         varAxis tells program which axis the movement is made on 
         """
-        if varAxis == 'y':
-            if varChange == '+ve':
-                listCoord[1]+=1
-            elif varChange == '-ve':
-                listCoord[1]-=1
+        if isFollowing:
+            if varAxis == 'y':
+                if varChange == '+ve':
+                    listCoord[1]+=1
+                elif varChange == '-ve':
+                    listCoord[1]-=1
+                else:
+                    print("Invalid changer.")
+                    sys.exit(1)
+            
+            elif varAxis == 'x':
+                if varChange == '+ve':
+                    listCoord[0]+=1
+                elif varChange == '-ve':
+                    listCoord[0]-=1
+                else:
+                    print("Invalid changer.")
+                    sys.exit(1)
+
             else:
-                print("Invalid changer.")
+                print("Invalid axis.")
                 sys.exit(1)
-        
-        elif varAxis == 'x':
-            if varChange == '+ve':
-                listCoord[0]+=1
-            elif varChange == '-ve':
-                listCoord[0]-=1
-            else:
-                print("Invalid changer.")
-                sys.exit(1)
-
-        else:
-            print("Invalid axis.")
-            sys.exit(1)
-        
-
-
-        
     
+    def checkProximity(self,playerCoord,proximal):
+        '''
+        checks for proximity of player
+        if Karen is within a distance defined by 'proximal' parameter, changes the value of isFollowing to True if not already True
+        '''
+        if not self.isFollowing:
+            
+        else:
+            pass
+
+
+
+class Goblin:
+    pass
